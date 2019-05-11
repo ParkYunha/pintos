40fe02a0111c92f75b68479a0bbcef382f5a871e
박윤하 <yunha@bag-yunhaui-MacBook-Pro.local>
2019-05-12 03:39:37 +0900

    swap.h add headers

--- vm/swap.h
+++ vm/swap.h
@@ -1,6 +1,26 @@
 #ifndef VM_SWAP_H
 #define VM_SWAP_H
 
+#include <stdint.h> //unint32_t
+#include <stdbool.h>
+#include <list.h>
+#include <hash.h>
+#include <debug.h>
+
+#include "vm/page.h"
+#include "vm/frame.h"
+
+#include "filesys/file.h"
+#include "threads/vaddr.h"
+#include "threads/thread.h"
+#include "threads/interrupt.h"
+#include "threads/malloc.h"
+#include "threads/palloc.h"
+
+#include "userprog/pagedir.h"
+#include "userprog/process.h"
+#include "userprog/syscall.h"
+
 void swap_init (void);
 bool swap_in (void *addr);
 bool swap_out (void);
